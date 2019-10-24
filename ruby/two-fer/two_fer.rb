6bad35c592cb497b900ef62a11a784180a92461e
Noah Flint <noah.flint24@gmail.com>
2019-10-23 19:34:33 -0600

    Refactored two_fer method default argument to you

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,11 +1,6 @@
 class TwoFer
 
-  def self.two_fer(name = '')
-    if name == ''
-      'One for you, one for me.'
-    else
-      "One for #{name}, one for me."
-    end
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
   end
-
 end
